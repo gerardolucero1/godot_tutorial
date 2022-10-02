8750cd60fe0d79d8542961471efc2e9e7e7dc1c1
Gerardo Lucero <gera_conecta@hotmail.com>
2022-10-02 17:27:37 -0600

    cositas

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+signal died
+
 var gravity = 1000
 var velocity = Vector2.ZERO
 var maxHorizontalSpeed = 140
@@ -9,7 +11,7 @@ var jumpTerminationMultiplier = 4
 var hasDoubleJump = false
 
 func _ready():
-	pass # Replace with function body.
+	$HazardArea.connect("area_entered", self, "on_hazard_area_entered")
 
 func _process(delta):
 	var moveVector = get_movement_vector()
@@ -60,3 +62,6 @@ func update_animation():
 	
 	if(moveVector.x != 0):
 		$AnimatedSprite.flip_h = true if moveVector.x > 0 else false
+		
+func on_hazard_area_entered(_area2d):
+	emit_signal("died")
