90a503262f37992c586e3b627da038b553244ae4
gerardolucero1 <gera_conecta@hotmail.com>
2022-10-13 16:01:54 -0600

    cositas

---
+++ scenes/Enemy.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+enum Direction { RIGHT, LEFT }
+export(Direction) var startDirection
+
+var maxSpeed = 50
+var velocity = Vector2.ZERO
+var direction = Vector2.ZERO
+var gravity = 500
+
+func _ready():
+	direction = Vector2.RIGHT if startDirection == Direction.RIGHT else Vector2.LEFT
+	$GoalDetector.connect("area_entered", self, "on_goal_detector")
+	$HitboxArea.connect("area_entered", self, "on_hitbox_entered")
+
+func _process(delta):
+	velocity.x = (direction * maxSpeed).x
+	#if(!is_on_floor()):
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	$AnimatedSprite.flip_h = true if direction.x > 0 else false
+
+func on_goal_detector(_area2d):
+	direction *= -1
+	
+func on_hitbox_entered(_area2d):
+	queue_free()
+	
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -2,18 +2,45 @@ extends KinematicBody2D
 
 signal died
 
+enum State {NORMAL, DASHING}
+
+export(int, LAYERS_2D_PHYSICS) var dashHazardMask
+
 var gravity = 1000
 var velocity = Vector2.ZERO
 var maxHorizontalSpeed = 140
+var maxDashSpeed = 500
+var minDashSpeed = 100
 var horizontalAcceleration = 2000
-var jumpSpeed = 360
+var jumpSpeed = 320
 var jumpTerminationMultiplier = 4
 var hasDoubleJump = false
+var hasDash = false
+var currentState = State.NORMAL
+var isStateNew = true
+var defaultHazardMask = 0
 
 func _ready():
 	$HazardArea.connect("area_entered", self, "on_hazard_area_entered")
+	defaultHazardMask = $HazardArea.collision_mask
 
 func _process(delta):
+	match currentState:
+		State.NORMAL:
+			process_normal(delta)
+		State.DASHING:
+			process_dash(delta)
+	isStateNew = false
+	
+func change_state(newState):
+	currentState = newState
+	isStateNew = true
+	
+func process_normal(delta):
+	if(isStateNew):
+		$DashArea/CollisionShape2D.disabled = true
+		$HazardArea.collision_mask = defaultHazardMask
+		
 	var moveVector = get_movement_vector()
 	
 	velocity.x += moveVector.x * horizontalAcceleration * delta
@@ -41,9 +68,35 @@ func _process(delta):
 		
 	if (is_on_floor()):
 		hasDoubleJump = true
+		hasDash = true
+		
+	if (hasDash && Input.is_action_just_pressed("dash")):
+		call_deferred("change_state", State.DASHING)
+		hasDash = false
 	
 	update_animation()
+
+func process_dash(delta):
+	if(isStateNew):
+		$DashArea/CollisionShape2D.disabled = false
+		$AnimatedSprite.play("jump")
+		$HazardArea.collision_mask = dashHazardMask
+		var moveVector = get_movement_vector()
+		var velocityMod = 1
+		
+		if(moveVector.x != 0):
+			velocityMod = sign(moveVector.x)
+		else:
+			velocityMod = 1 if $AnimatedSprite.flip_h else -1
+			
+		velocity = Vector2(maxDashSpeed * velocityMod, 0)
 	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity.x = lerp(0, velocity.x, pow(2, -8 * delta))
+	
+	if(abs(velocity.x) < minDashSpeed):
+		call_deferred("change_state", State.NORMAL)
+
 func get_movement_vector():
 	var moveVector = Vector2.ZERO
 	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
