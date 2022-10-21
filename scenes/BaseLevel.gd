3fd4afe0209df165e9ba6d95f5e68d20790c919d
gerardolucero1 <gera_conecta@hotmail.com>
2022-10-21 15:59:10 -0600

    cositas

--- scenes/BaseLevel.gd
+++ scenes/BaseLevel.gd
@@ -1,13 +1,27 @@
 extends Node
 
+signal coin_total_changed
 
 var playerScene = preload("res://scenes/Player.tscn")
 var spawnPosition = Vector2.ZERO
 var currentPlayerNode = null
+var totalCoins = 0
+var collectedCoins = 0
 
 func _ready():
 	spawnPosition = $Player.global_position
 	register_player($Player)
+	
+	coin_total_changed(get_tree().get_nodes_in_group("coin").size())
+	
+func coin_collected():
+	collectedCoins += 1
+	print(totalCoins, ' - ' ,collectedCoins)
+	emit_signal("coin_total_changed", totalCoins, collectedCoins)
+	
+func coin_total_changed(newTotal):
+	totalCoins = newTotal
+	emit_signal("coin_total_changed", totalCoins, collectedCoins)
 	
 func register_player(player):
 	currentPlayerNode = player
--- scenes/Coin.gd
+++ scenes/Coin.gd
@@ -7,5 +7,8 @@ func on_area_entered(_area2d):
 	$AnimationPlayer.play("pickup")
 	call_deferred("disable_pickup")
 	
+	var baseLevel = get_tree().get_nodes_in_group("base_level")[0]
+	baseLevel.coin_collected()
+	
 func disable_pickup():
 	$Area2D/CollisionShape2D.disabled = true
